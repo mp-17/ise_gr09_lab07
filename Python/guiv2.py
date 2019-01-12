7210adf9e2e00ef376f4baa6d598b0c36106c5e6
Giuseppe <s255648@studenti.polito.it>
2019-01-12 19:33:02 +0100

    some fixes in guiv2

--- Python/guiv2.py
+++ Python/guiv2.py
@@ -47,11 +47,11 @@ def saveResult(result):
             for col in range(1,3):
                 saveWindow.columnconfigure(col, weight=1)
             saveWindow.rowconfigure(row, weight=1)
-    name=tk.Label(saveWindow,textvariable="Name:",bg='white',fg='black')
+    name=tk.Label(saveWindow,text="Name:",bg='white',fg='black',font="Helvetica 25 bold",justify="center")
     name.grid(row=1,column=1,sticky=(W))
     nick=tk.Entry(saveWindow,bg='white',fg='black')
     nick.grid(row=1,column=2,sticky=(E))
-    yesButton=tk.Button(saveWindow,text="Yes", command=lambda: uploadMem(nick.get(),result,saveWindow), bg='red3', highlightbackground="firebrick4", font="Helvetica 20 bold ", activebackground="SkyBlue1", highlightthickness=4, pady=5, width=5, height=1)
+    yesButton=tk.Button(saveWindow,text="Yes", command=lambda: uploadMem(nick.get(),result,saveWindow), bg='red3', highlightbackground="firebrick4", font="Helvetica 20 bold ", activebackground="SkyBlue1", highlightthickness=4, pady=5, width=6, height=1)
     yesButton.grid(row=2,column=1,sticky=(N, S, W, E))
     noButton=tk.Button(saveWindow,text="No", command=lambda:quit_window(saveWindow), bg='red3', highlightbackground="firebrick4", font="Helvetica 20 bold ", activebackground="SkyBlue1", highlightthickness=4, pady=5, width=5, height=1)
     noButton.grid(row=2,column=2,sticky=(N, S, W, E))
@@ -125,7 +125,7 @@ def displayLeaderboard():
                         pady=5,
                         width=3,
                         height=1)
-    quit_button.grid(row=i, columnspan=3, sticky= (N, S, W, E))
+    quit_button.grid(row=i, columnspan=4, sticky= (N, S, W, E))
 ####
 
 root= Tk()
@@ -164,7 +164,7 @@ t_score_lab=tk.Label(mainFrame, textvariable=t_score, font="Helvetica 35 bold", 
 error_lab=tk.Label(mainFrame, textvariable=error_flag,  font="Helvetica 25 bold", bg='white', fg='red', justify='center')
 
 ## save result
-save=tk.Button(mainFrame, text="Save", command=lambda: saveResult(t_score_lab.cget("text")), bg='red3', highlightbackground="firebrick4", font="Helvetica 20 bold ", activebackground="SkyBlue1", highlightthickness=4, pady=5, width=12, height=1)
+save=tk.Button(mainFrame, text="Save", command=lambda: saveResult(t_score_lab.cget("text")), bg='blue', highlightbackground="dark blue", font="Helvetica 20 bold ", activebackground="SkyBlue1", highlightthickness=4, pady=5, width=12, height=1)
 
 ## display leaderboard
 leaderboard=tk.Button(mainFrame, text="Leaderboard", command=displayLeaderboard, bg='red3', highlightbackground="firebrick4", font="Helvetica 20 bold ", activebackground="SkyBlue1", highlightthickness=4, pady=5, width=12, height=1)
@@ -187,7 +187,7 @@ t_score_lab.grid(row=2, column=2, sticky=(W))
 t_score_lab.grid_rowconfigure(2, weight=1)
 t_score_lab.grid_columnconfigure(2, weight=1)
 
-error_lab.grid(row=2, column=3, sticky=(N, E, S, W))
+error_lab.grid(row=1, column=2, sticky=(N, E, S, W))
 error_lab.grid_rowconfigure(2, weight=1)
 error_lab.grid_columnconfigure(3, weight=1)
 
@@ -199,6 +199,8 @@ led_off.grid_rowconfigure(1,weight=1)
 led_off.grid_columnconfigure(3,weight=2)
 
 save.grid(row=2, column=3, sticky=(N, S, E, W))
+save.grid_rowconfigure(2,weight=1)
+save.grid_columnconfigure(3, weight=1)
 leaderboard.grid(row=3, column=1, sticky=N)
 quit_button.grid(row=3, column=2, sticky= S)
 
