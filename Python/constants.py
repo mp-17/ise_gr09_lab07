2ebe5a7b2681734a37a927ae6a563db79d2968d3
mp-17 <twisted@live.it>
2019-01-31 18:07:10 +0100

    it works

--- Python/constants.py
+++ Python/constants.py
@@ -1,7 +1,7 @@
 #This file contains constants used by lib.module
 debug_constant = 1 #debug constant
 commands_to_send = ['L0', 'L1'] #list of possible commands to send to the serial port
-serial_port_name = '/dev/ttyACM0' #name of the used serial port
+serial_port_name = '/dev/ttyUSB0' #name of the used serial port
 maxLeaders=10
 mem_file="mem/MemCard1.txt"
 gui_mod="GUI"
